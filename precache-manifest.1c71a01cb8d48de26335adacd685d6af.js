d5d6c4d43b69c046245e8a5b20d578ab6ee856ac
ms0223900 <ms0223900@gmail.com>
2020-08-25 19:59:52 +0800

    Updates

---
+++ precache-manifest.1c71a01cb8d48de26335adacd685d6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1301b54c77324e1dc9ccd2bf90c6ee6",
+    "url": "/ptx-map-sample/index.html"
+  },
+  {
+    "revision": "4f85a6f31f1aff754a1e",
+    "url": "/ptx-map-sample/static/css/main.bfa977e1.chunk.css"
+  },
+  {
+    "revision": "cfd3dfbb48df22cfb5cf",
+    "url": "/ptx-map-sample/static/js/2.4375258b.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/ptx-map-sample/static/js/2.4375258b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f85a6f31f1aff754a1e",
+    "url": "/ptx-map-sample/static/js/main.73147d0d.chunk.js"
+  },
+  {
+    "revision": "263b3fcd62b088d7efdd",
+    "url": "/ptx-map-sample/static/js/runtime-main.c84815cb.js"
+  }
+]);
